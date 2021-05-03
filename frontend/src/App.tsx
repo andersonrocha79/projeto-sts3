a4a7d30dc2bb15b09590e28c5efe0614a8134095
Anderson Rocha <anderson_rocha79@hotmail.com>
2021-05-03 20:47:35 -0300

    componentes básicos estáticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import DataTable from "components/DataTable";
+import React from "react";
+
 function App() 
 {
   return (
-    <div className="App">
-        <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+        <NavBar />
+        <div className="container">
+            <h1 className="text-primary">Olá mundo!</h1> 
+            <DataTable />           
+        </div>
+        <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => 
+{
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/andersonrocha79" target="_blank" rel="noreferrer">Anderson Rocha</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
