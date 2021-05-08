9aa2e225941e7f9e5b7769c1b5baafcc6579e135
Anderson Rocha <anderson_rocha79@hotmail.com>
2021-05-08 11:42:40 -0300

    ajustes

--- backend/src/main/java/br/com/rochasoft/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/br/com/rochasoft/dsvendas/DsvendasApplication.java
@@ -1,5 +1,14 @@
 package br.com.rochasoft.dsvendas;
 
+/*
+criando aplicação no heroku
+heroku -v
+heroku login
+heroku git:remote -a <nome-do-app>
+git remote -v
+git subtree push --prefix backend heroku main
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
